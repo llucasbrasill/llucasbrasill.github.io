98de8af4dde7a88223ac82ce4065d1aad46f14c7
Lucas P. Sodré <llucasbrasill@gmail.com>
2020-03-15 16:36:52 -0300

    Add files via upload

---
+++ precache-manifest.b0578086c9f70998f031e393462b6cc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ba6aeadc14c2f7983196c077c47096",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a61a5f7288b1ae9ee714",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "a61a5f7288b1ae9ee714",
+    "url": "/static/js/2.d01d0c03.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/static/js/2.d01d0c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbf94b389eadc56edb1",
+    "url": "/static/js/main.b318f0e1.chunk.js"
+  },
+  {
+    "revision": "d644ff198816bb9f0669",
+    "url": "/static/js/runtime-main.c0627188.js"
+  }
+]);
